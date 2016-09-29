7ec6ee31c33fb2a4f526bbedb2838588f79505cb
mrb1805 <maahesh.rb@gmail.com>
2016-09-29 12:54:27 -0700

    POST

--- src/com/jpmc/resource/LearningRestApi.java
+++ src/com/jpmc/resource/LearningRestApi.java
@@ -1,12 +1,20 @@
 package com.jpmc.resource;
 
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.MatrixParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.PathSegment;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 
 @Path("/learning")
 
@@ -58,23 +66,40 @@ public class LearningRestApi
 				}
 				
 				//http://localhost:8080/Proj/rest/learning/ex05/1001/.../10390/action/Akshay
-
 				//http://localhost:8080/Proj/rest/learning/ex05/1001;dinner=free/1234;dinner=pay/1003;privilage=homedrop/../10390/action/Akshay
 				
 				@Path("/ex05/{empnos: .+}/action/{action}")
 				@GET
 				@Produces("text/plain")
-				public String example05(@PathParam("empnos") String empnos,
-						@PathParam("action") String action,
-						@MatrixParam("dinner") String dinner,
-						@MatrixParam("privilage") String privilage)
+				public String example05(@PathParam("empnos") List<PathSegment>	empnos,
+						@PathParam("action") String action)
 				{
-					System.out.println(empnos);
+					for(PathSegment empno:empnos){
+						System.out.println(empno.getPath());
+						System.out.println(empno.getMatrixParameters());
+					}
 					System.out.println(action);
-					System.out.println(dinner);
-					System.out.println(privilage);
 					//add your own println to confirm the working
 					return "Response from example 05";
 					
-				}		
+				}
+				
+				
+				//http://localhost:8080/Proj/rest/learning/ex06
+				//In Rest Client Body name=Mahesh&email=Majrul@gmail.com
+				
+				@Path("/ex06")
+				@POST
+				@Consumes("application/x-www-form-urlencoded")
+				@Produces("text/plain")
+				public String example06(
+						@FormParam("name") String name,
+						@FormParam("email") String email){
+					
+				System.out.println("name: "+ name+ "email: "+email);
+				
+				return "Response of Ex06";
+			
+				}
+	
 }
